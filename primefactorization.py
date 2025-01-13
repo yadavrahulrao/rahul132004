03da247052dd56fa2eea186e5ae144b6f0be63db
RAHUL <rahul507538@gmail.com>
2025-01-13 16:16:54 +0530

    Add files via upload

---
+++ primefactorization.py
@@ -0,0 +1,28 @@
+# to find factors of a number 
+
+import math
+
+
+def factors(n):
+	
+
+	while n % 2 == 0:
+		print(2)
+		n = n // 2
+		
+
+	for i in range(3,int(math.sqrt(n))+1,2):
+		
+		while n % i== 0:
+			print(i)
+			n = n // i
+			
+
+	if n > 2:
+		print(n)
+		
+
+n = int(input("enter number: "))
+factors(n)
+
+
